8131a108bd965c4e2a572ce3529729b93fa82493
Pratik Mishra <74451989+DolceParadise@users.noreply.github.com>
2022-02-09 02:37:33 +0530

    Added get_position

--- gym-line-follower-env/gym_line_follower/helper.py
+++ gym-line-follower-env/gym_line_follower/helper.py
@@ -1,21 +1,26 @@
-import numpy 
+import numpy
 import gym_line_follower
 import gym
 import time
-import pybullet as p 
+import pybullet as p
 import pybullet_data
+
 
 class LaRoboLigaEnv(gym.Env):
     def __init__(self):
         self.env = gym.make("LineFollower-v0")
-        _,self.path=self.env.reset()
+        _, self.path = self.env.reset()
 
     def get_full_path(self):
         return self.path
 
     def reset(self):
-        _,self.path=self.env.reset()
+        _, self.path = self.env.reset()
 
-    def set_velocity(self,array):
-        obs,reward,done,info=self.env.step(array)
-        return info,done
+    def set_velocity(self, array):
+        self.observation, self.reward, self.done, self.info = self.env.step(
+            array)
+
+    def get_position(self):
+        position = self.info
+        return position
